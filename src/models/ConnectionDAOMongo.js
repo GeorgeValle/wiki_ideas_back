db7522747acec25a35aa983adbab25b503b81559
GeorgeValle <jorgeguillermovalle@gmail.com>
2023-06-12 20:28:48 -0300

    (feat): add  fly.toml and dockerfile
    archives for config deploy in container and fly.io hosting

--- src/models/ConnectionDAOMongo.js
+++ src/models/ConnectionDAOMongo.js
@@ -1,21 +1,26 @@
 //*** this file export the connections to alls DAOS ***/
 import dotenv from 'dotenv';
 dotenv.config();
-import mongoose, {mongo} from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 const uri= process.env.MONGO_URI
-const ear= mongoose.connection;
+const ear = mongoose.connection;
 
+
+
+//mongoose.connect(`mongodb://db:27017/${process.env.MONGO_DATABASE}`,
 mongoose.connect(uri,
     {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false
-}).catch(err => {console.log(err)})
+        // user: process.env.MONGO_USERNAME,
+        // pass: process.env.MONGO_PASSWORD,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false
+    }).catch(err => { console.log(err) })
 
-ear.once('open',_=>{
+ear.once('open', _ => {
     console.log(`Database is connected to: `, uri)
 })
 
-ear.on('error', err => {console.log(`Type error: ${err}`)})
+ear.on('error', err => { console.log(`Type error: ${err}`) })
